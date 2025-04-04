dfba1bd86e3e709cc49ee9f5668d2f1086f4f1e2
wbxzhwl <3210633900@qq.com>
2025-04-04 13:34:06 +0800

    Add two folders using Git LFS

---
+++ acneClassification/train.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c4ecb7d92f0ed08c1faaf86dec42f3139528fb4802a733e2785d2bff3dd909f
+size 10316
---
+++ acneClassification/web.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b368ade3e10e921735db02ae45737d18e260e2da96e1a84f57c2bfd313244d83
+size 4546
